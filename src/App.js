5a10debb98157cde1aebee71262970cba5eba949
Vaishal Parikh <86185385+vaishalparikh@users.noreply.github.com>
2022-07-24 02:59:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Resturant from "./component/Basics/Resturant"; 
+// import UseState from "./component/Hooks/useState";
+// import UseEffect from "./component/Hooks/useEffect";
+// import UseReducer from "./component/Hooks/useReducer";
+
+const App = () => {
+  return <Resturant />;
+};
+
+export default App;
+
+
+
+
